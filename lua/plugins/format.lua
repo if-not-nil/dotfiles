424a5f26e8a5d57a95b6ff926671660024a014bd
qwool rat <qwool@proton.me>
2025-05-11 10:06:17 +0300

    checkpoint

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -1,30 +1,19 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-
-		conform.setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "biome", stop_after_first = true },
-				typescript = { "biome", "prettier", stop_after_first = true },
-				javascriptreact = { "biome", "prettier", stop_after_first = true },
-				typescriptreact = { "biome", stop_after_first = true },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
-		})
-
-		-- vim.keymap.set({ "n", "v" }, "<leader>fm", function()
-		-- 	conform.format({
-		-- 		lsp_fallback = true,
-		-- 		async = false,
-		-- 		timeout_ms = 1000,
-		-- 	})
-		-- end, { desc = "Format file or range (in visual mode)" })
-	end,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { "biome", stop_after_first = true },
+			typescript = { "biome", "prettier", stop_after_first = true },
+			javascriptreact = { "biome", "prettier", stop_after_first = true },
+			typescriptreact = { "biome", stop_after_first = true },
+			-- python = { "ruff" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		},
+	},
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,89 +6,30 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		{ "j-hui/fidget.nvim", opts = {} },
 	},
-	config = function()
+	opts = {
+		servers = {},
+	},
+	config = function(_, opts)
 		local lspconfig = require("lspconfig")
-		local mason_lspconfig = require("mason-lspconfig")
-		local capabilities = require("blink.cmp").get_lsp_capabilities({
-			textDocument = {
-				foldingRange = {
-					dynamicRegistration = false,
-					lineFoldingOnly = true,
-				},
-			},
-		})
+		for server, config in pairs(opts.servers) do
+			-- passing config.capabilities to blink.cmp merges with the capabilities in your
+			-- `opts[server].capabilities, if you've defined it
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+			lspconfig[server].setup(config)
+		end
 
 		require("mason").setup()
-
-		mason_lspconfig.setup({
-			handlers = {
-				-- Default handler
-				function(server_name)
-					lspconfig[server_name].setup({
-						capabilities = capabilities,
-					})
-				end,
-
-				-- Pylsp custom config
-				pylsp = function()
-					lspconfig.pylsp.setup({
-						capabilities = capabilities,
-						settings = {
-							pylsp = {
-								plugins = {
-									pyflakes = { enabled = false },
-									mccabe = { enabled = false },
-								},
-							},
-						},
-					})
-				end,
-
-				-- Svelte workaround for watcher issue
-				svelte = function()
-					lspconfig.svelte.setup({
-						filetypes = { "svelte" },
-						capabilities = capabilities,
-						on_attach = function(client, bufnr)
-							if client.name == "svelte" and vim.bo[bufnr].filetype == "svelte" then
-								vim.api.nvim_create_autocmd("BufWritePost", {
-									pattern = { "*.js", "*.ts", "*.svelte" },
-									callback = function(ctx)
-										client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.file })
-									end,
-								})
-							end
-						end,
-					})
-				end,
-
-				-- Lua LSP with Neovim globals and runtime
-				lua_ls = function()
-					lspconfig.lua_ls.setup({
-						cmd = { "lua-language-server" },
-						filetypes = { "lua" },
-						capabilities = capabilities,
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = {
-										vim.api.nvim_get_runtime_file("", true),
-										"${3rd}/love2d/library",
-									},
-								},
-								telemetry = { enable = false },
-							},
-						},
-					})
-				end,
+		require("mason-lspconfig").setup({
+			automatic_enable = {
+				exclude = {
+					"ruff",
+				},
 			},
-		})
-
-		vim.diagnostic.config({
-			virtual_text = true,
+			ensure_installed = { "lua_ls" },
 		})
 	end,
+
+	vim.diagnostic.config({
+		virtual_text = true,
+	}),
 }
